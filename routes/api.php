b70ab666c2c7fee4362b29ba709e85086eb5868f
devdiamond9703 <devdiamond9703@gmail.com>
2023-10-03 10:35:10 +0500

    init

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AttributeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,7 +21,11 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
+Route::get('/test/{id}', function (Request $request, App\Models\User $id) {
+     return new \Illuminate\Http\JsonResponse([
+         'data => ' => $id
+     ]);
+ });
 Route::get('attributes', [AttributeController::class, 'index']);
 Route::post('attributes', [AttributeController::class, 'store']);
 Route::get('attributes/value/{id}', [AttributeController::class, 'valuesByAttr']);
